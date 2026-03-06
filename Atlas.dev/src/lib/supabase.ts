d9be70c5fa8ebf29724e3453147c6b7bf3ac9ee3
LoubnaAbbar <loubna.abbar@edu.devinci.fr>
2026-03-06 10:37:10 +0100

    Info

--- Atlas.dev/src/lib/supabase.ts
+++ Atlas.dev/src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://yrmmxiqgevzhdpbdkwvy.supabase.co';  // colle directement
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlybW14aXFnZXZ6aGRwYmRrd3Z5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI0NDkzNDUsImV4cCI6MjA4ODAyNTM0NX0.2gY16mKRkvHDZcca-THh7e4iV6VT2bvTGRx6LkY1aTc';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
